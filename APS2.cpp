1b7f7fcd3491f20028a944c3d88e3b4016aa57ab
Vinicius Nobrega Costa <viniciusnobcosta@gmail.com>
2022-10-27 16:21:20 -0300

    1 more change

--- APS2.cpp
+++ APS2.cpp
@@ -1,3 +1,7 @@
+//Nome da disciplina: Programação Estruturada
+//Código da Turma: 931 
+//Nome e Matricula: Vinicius Nobrega Costa | 2022100111
+
 #include<stdio.h>
 #include<stdlib.h>
 #include<locale.h>
